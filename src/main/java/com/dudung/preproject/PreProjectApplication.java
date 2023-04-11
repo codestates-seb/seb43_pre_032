9d3e2f821dd0d4cfb73733bf92aa974cb23f5f1d
RapiL <sjw3090gmail.com>
2023-04-11 17:24:51 +0900

    test

---
+++ src/main/java/com/dudung/preproject/PreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dudung.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dudung/preproject/PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudung.preproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
