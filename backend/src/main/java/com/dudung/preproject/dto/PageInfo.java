b85d4c55c4862a13aab4297fa38c7a5fd55e2d57
DDunDDang <thdwns0331@gmail.com>
2023-04-14 16:24:15 +0900

    PageNation을 위한 MultiResponseDto, PageInfo 작성

---
+++ backend/src/main/java/com/dudung/preproject/dto/PageInfo.java
@@ -0,0 +1,13 @@
+package com.dudung.preproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
