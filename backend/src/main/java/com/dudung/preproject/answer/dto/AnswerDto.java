060f7688a8f9ef390d92e34b9711cf174e4d0208
RapiL <sjw3090gmail.com>
2023-04-14 15:13:40 +0900

    AnswerDto 주석 해제 및 Response 수정 AnswerMapper Mapping 설정

--- backend/src/main/java/com/dudung/preproject/answer/dto/AnswerDto.java
+++ backend/src/main/java/com/dudung/preproject/answer/dto/AnswerDto.java
@@ -21,23 +21,21 @@ public class AnswerDto {
     @Setter
     public static class Patch {
 
-//        private Long memberId;
+        private Long memberId;
 
         private Long answerId;
 
         private Question question;
 
-//        private String answerContent;
+        private String answerContent;
 
-//        private LocalDateTime modifiedAt = LocalDateTime.now();
+        private LocalDateTime modifiedAt = LocalDateTime.now();
     }
     @Getter
     public static class Response {
-        // Ansewr의 Response에 들어갈 내용이 뭐가 있는지 ??
-        // 응답 데이터로 뭔가 한다던가 혹은 완료 메세지를 해야하는가?
         private Long answerId;
         private Question question;
-        private Member member;
+        private String memberName;
         private String answerContent;
         private LocalDateTime modifiedAt;
 
