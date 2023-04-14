8867a9625d836bda3df034aeba47cdf68fed969c
DDunDDang <thdwns0331@gmail.com>
2023-04-14 14:35:14 +0900

    MemberAnswerVote 수정

--- backend/src/main/java/com/dudung/preproject/memberanswer/domain/MemberAnswer.java
+++ backend/src/main/java/com/dudung/preproject/memberanswer/domain/MemberAnswer.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -16,5 +17,5 @@ public class MemberAnswer {
     private Long memberAnswerId;
 
     @OneToMany
-    private List<Answer> answers;
+    private List<Answer> answers = new ArrayList<>();
 }
