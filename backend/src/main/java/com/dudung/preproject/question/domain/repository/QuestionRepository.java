79cbf933a42bbb4cec30d49a24e6c03df7acf9d8
DDunDDang <thdwns0331@gmail.com>
2023-04-14 12:11:40 +0900

    QuestionRepository 경로 수정

--- backend/src/main/java/com/dudung/preproject/question/domain/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dudung.preproject.question.domain.repository;
-
-import com.dudung.preproject.question.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Long, Question> {
-}
