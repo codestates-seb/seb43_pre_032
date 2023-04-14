b41d8dda6d52599106d5b52507d7141a07a0c590
DDunDDang <thdwns0331@gmail.com>
2023-04-14 12:18:07 +0900

    Exception code "Question not found" 추가

--- backend/src/main/java/com/dudung/preproject/exception/ExceptionCode.java
+++ backend/src/main/java/com/dudung/preproject/exception/ExceptionCode.java
@@ -6,7 +6,8 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EMAIL_EXISTS(409, "email exists"),
     MEMBER_NAME_EXISTS(409, "name exists"),
-    ANSWER_NOT_FOUND(404, "Answer not found");
+    ANSWER_NOT_FOUND(404, "Answer not found"),
+    QUESTION_NOT_FOUND(404, "Question not found");
 
     @Getter
     private int status;
