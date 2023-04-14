6308ba3a5321c9c25fc045a9d29aec65a3c2ce50
DDunDDang <thdwns0331@gmail.com>
2023-04-14 16:07:21 +0900

    QuestionDto 수정

--- backend/src/main/java/com/dudung/preproject/question/dto/QuestionDto.java
+++ backend/src/main/java/com/dudung/preproject/question/dto/QuestionDto.java
@@ -1,14 +1,21 @@
 package com.dudung.preproject.question.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 public class QuestionDto {
+    @Getter
     public static class Post {
         private long memberId;
         private String questionTitle;
         private String questionContent;
         private LocalDateTime createdAt = LocalDateTime.now();
     }
+    @Getter
+    @Setter
     public static class Patch {
         private long memberId;
         private long questionId;
@@ -17,6 +24,8 @@ public class QuestionDto {
         private LocalDateTime modifiedAt = LocalDateTime.now();
     }
 
+    @Getter
+    @Builder
     public static class Response {
         private String questionTitle;
         private String questionContent;
