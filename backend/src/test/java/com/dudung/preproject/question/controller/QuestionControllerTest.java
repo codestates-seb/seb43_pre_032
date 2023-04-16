3a22bf1060c9f9f6d88a6343e8f32fb0a0e4104a
DDunDDang <thdwns0331@gmail.com>
2023-04-16 21:23:02 +0900

    QuestionControllerTest 수정 - Documentation 코드 작성

--- backend/src/test/java/com/dudung/preproject/question/controller/QuestionControllerTest.java
+++ backend/src/test/java/com/dudung/preproject/question/controller/QuestionControllerTest.java
@@ -1,7 +1,6 @@
 package com.dudung.preproject.question.controller;
 
 import com.dudung.preproject.member.service.MemberService;
-import com.dudung.preproject.question.controller.QuestionController;
 import com.dudung.preproject.question.domain.Question;
 import com.dudung.preproject.question.dto.QuestionDto;
 import com.dudung.preproject.question.dto.QuestionResponseDto;
@@ -20,7 +19,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.LinkedMultiValueMap;
@@ -31,17 +33,25 @@ import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.dudung.preproject.utils.ApiDocumentUtils.getRequestPreProcessor;
+import static com.dudung.preproject.utils.ApiDocumentUtils.getResponsePreProcessor;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(QuestionController.class)
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureRestDocs
+@MockBean(JpaMetamodelMappingContext.class)
 public class QuestionControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -75,7 +85,21 @@ public class QuestionControllerTest {
         actions
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", is(startsWith("/questions"))))
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("post-question",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별 번호"),
+                                        fieldWithPath("questionTitle").type(JsonFieldType.STRING).description("질문 제목"),
+                                        fieldWithPath("questionContent").type(JsonFieldType.STRING).description("질문 내용")
+                                )
+                        ),
+                        responseHeaders(
+                                headerWithName(HttpHeaders.LOCATION).description("Location header. 등록된 리소스의 URI")
+                        )
+                ));
     }
 
     @Test
@@ -123,7 +147,34 @@ public class QuestionControllerTest {
                 .andExpect(jsonPath("$.question.questionVoteSum").value(0))
                 .andExpect(jsonPath("$.question.viewCount").value(0))
                 .andExpect(jsonPath("$.question.memberName").value("두둥탁"))
-                .andDo(print());
+                .andDo(print())
+                .andDo(document("patch-question",
+                        getRequestPreProcessor(),
+                        getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("question-id").description("질문 식별자")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별 번호").optional(),
+                                        fieldWithPath("questionId").type(JsonFieldType.NUMBER).description("질문 식별 번호").ignored(),
+                                        fieldWithPath("questionTitle").type(JsonFieldType.STRING).description("질문 제목").optional(),
+                                        fieldWithPath("questionContent").type(JsonFieldType.STRING).description("질문 내용").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("question.questionTitle").type(JsonFieldType.STRING).description("질문 제목").optional(),
+                                        fieldWithPath("question.questionContent").type(JsonFieldType.STRING).description("질문 내용").optional(),
+                                        fieldWithPath("question.createdAt").type(JsonFieldType.STRING).description("질문 생성 시간").optional(),
+                                        fieldWithPath("question.modifiedAt").type(JsonFieldType.STRING).description("질문 마지막 수정 시간").optional(),
+                                        fieldWithPath("question.questionVoteSum").type(JsonFieldType.NUMBER).description("질문 투표 합계").optional(),
+                                        fieldWithPath("question.viewCount").type(JsonFieldType.NUMBER).description("질문 조회수").optional(),
+                                        fieldWithPath("question.memberName").type(JsonFieldType.STRING).description("회원 이름").optional(),
+                                        fieldWithPath("answer").type(JsonFieldType.STRING).description("답변").optional()
+                                )
+                        )
+                ));
     }
 
     @Test
