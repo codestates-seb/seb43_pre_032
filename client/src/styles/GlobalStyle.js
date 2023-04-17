02eddde0b2bb2dbe271942b45049ca4d69d63ee1
JHJFE <ghwo9611@gmail.com>
2023-04-17 13:49:41 +0900

    feature nav

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -6,10 +6,10 @@ const GlobalStyle = createGlobalStyle`
     --login-btn-before:hsl(206,96%,90%);
     --login-btn-after:hsl(206,93%,83.5%);
     --login-btn-border: hsl(205,47%,42%);;
-
     --signup-btn-before:hsl(206,100%,52%);
     --signup-btn-after:hsl(206,100%,40%);
     --menu-hover-background:hsl(210,8%,90%);
+    
 }
 `;
 
