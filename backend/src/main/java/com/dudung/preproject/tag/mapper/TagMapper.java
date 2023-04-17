2d6d68e372fb8db7f265c9eee85634604c0f9a27
DDunDDang <thdwns0331@gmail.com>
2023-04-17 17:50:46 +0900

    TagMapper 수정 - Tag 단일 요청에 대한 Response 추가

--- backend/src/main/java/com/dudung/preproject/tag/mapper/TagMapper.java
+++ backend/src/main/java/com/dudung/preproject/tag/mapper/TagMapper.java
@@ -1,12 +1,39 @@
 package com.dudung.preproject.tag.mapper;
 
+import com.dudung.preproject.question.domain.Question;
+import com.dudung.preproject.question.dto.QuestionDto;
 import com.dudung.preproject.tag.domain.Tag;
 import com.dudung.preproject.tag.dto.TagDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface TagMapper {
     List<TagDto.ResponseForList> tagsToTagsResponse(List<Tag> tagList);
+
+    default TagDto.Response tagTotagResponse(Tag tag, List<Question> questions) {
+        List<QuestionDto.ResponseForList> responseQuestions = questions.stream()
+                .map(question -> questionToQuestionResponseForList(question))
+                .collect(Collectors.toList());
+        return TagDto.Response.builder()
+                .tagId(tag.getTagId())
+                .tagName(tag.getTagName())
+                .tagDescription(tag.getTagDescription())
+                .questions(responseQuestions).build();
+    }
+
+    default QuestionDto.ResponseForList questionToQuestionResponseForList(Question question) {
+        return QuestionDto.ResponseForList.builder()
+                .questionTitle(question.getQuestionTitle())
+                .viewCount(question.getViewCount())
+                .questionVoteSum(question.getQuestionVoteSum())
+                .createdAt(question.getCreatedAt())
+                .tagName(tagsToTagNames(question.getTags()))
+                .memberName(question.getMember().getName())
+                .answerCount(question.getAnswerCount()).build();
+    }
+
+    List<TagDto.Name> tagsToTagNames(List<Tag> tagList);
 }
