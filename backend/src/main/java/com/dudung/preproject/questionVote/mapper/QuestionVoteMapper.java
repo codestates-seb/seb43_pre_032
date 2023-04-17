ac496465ce4d4caa73018101265b26680d8fed73
swj8560 <swj5514@gmail.com>
2023-04-17 11:04:31 +0900

    QuestionVoteMapper 수정

--- backend/src/main/java/com/dudung/preproject/questionVote/mapper/QuestionVoteMapper.java
+++ backend/src/main/java/com/dudung/preproject/questionVote/mapper/QuestionVoteMapper.java
@@ -2,9 +2,11 @@ package com.dudung.preproject.questionVote.mapper;
 
 import com.dudung.preproject.questionVote.domain.QuestionVote;
 import com.dudung.preproject.questionVote.dto.QuestionVoteDto;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface QuestionVoteMapper {
     QuestionVote questionVotePostDtoToQuestionVote(QuestionVoteDto.QuestionVotePost requestBody);
     QuestionVote questionVotePatchDtoToQuestionVote(QuestionVoteDto.QuestionVotePatch requestBody);
-    QuestionVoteDto.QuestionVoteResponse questionVoteToQuestionVoteResponse(QuestionVoteDto QuestionVoteDto);
+    QuestionVoteDto.QuestionVoteResponse questionVoteToQuestionVoteResponse(QuestionVote QuestionVote);
 }
