3ff846f0070d4ad10bce4ebc897b7f723b7d1647
DDunDDang <thdwns0331@gmail.com>
2023-04-18 02:16:26 +0900

    QuestionRepository 수정 - QuestionTag 가 추가되어 기존 로직대로 동작하도록 쿼리문 추가

--- backend/src/main/java/com/dudung/preproject/question/repository/QuestionRepository.java
+++ backend/src/main/java/com/dudung/preproject/question/repository/QuestionRepository.java
@@ -1,13 +1,16 @@
 package com.dudung.preproject.question.repository;
 
 import com.dudung.preproject.question.domain.Question;
+import com.dudung.preproject.question.domain.QuestionTag;
 import com.dudung.preproject.tag.domain.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Page<Question> findAllByTagsContains(Tag tag, Pageable pageable);
+    @Query("SELECT q FROM Question q JOIN q.questionTags c WHERE c.tag = :tag")
+    Page<Question> findAllByTag(Tag tag, Pageable pageable);
 }
