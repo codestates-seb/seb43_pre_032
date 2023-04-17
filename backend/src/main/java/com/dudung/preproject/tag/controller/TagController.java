d4fe5a35eb6494f7f9c8a5f80036fba8f3fd0fd4
DDunDDang <thdwns0331@gmail.com>
2023-04-17 17:52:28 +0900

    TagController 수정 - GetMapping("{tag-id}")에 해당하는 Get 요청 응답 메서드 추가

--- backend/src/main/java/com/dudung/preproject/tag/controller/TagController.java
+++ backend/src/main/java/com/dudung/preproject/tag/controller/TagController.java
@@ -1,6 +1,10 @@
 package com.dudung.preproject.tag.controller;
 
 import com.dudung.preproject.dto.MultiResponseDto;
+import com.dudung.preproject.dto.TagQuestionListResponseDto;
+import com.dudung.preproject.question.domain.Question;
+import com.dudung.preproject.question.mapper.QuestionMapper;
+import com.dudung.preproject.question.service.QuestionService;
 import com.dudung.preproject.tag.domain.Tag;
 import com.dudung.preproject.tag.mapper.TagMapper;
 import com.dudung.preproject.tag.service.TagService;
@@ -8,10 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Positive;
 import java.util.List;
@@ -21,14 +22,25 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TagController {
     private final TagService tagService;
-    private final TagMapper mapper;
+    private final TagMapper tagMapper;
+    private final QuestionService questionService;
+    private final QuestionMapper questionMapper;
 
     @GetMapping
     public ResponseEntity getTags(@Positive @RequestParam int page, @Positive @RequestParam int size, @RequestParam String sortBy) {
         Page<Tag> pageTags = tagService.findTags(page - 1, size, sortBy);
         List<Tag> tags = pageTags.getContent();
 
-        return new ResponseEntity<>(new MultiResponseDto<>(mapper.tagsToTagsResponse(tags), pageTags), HttpStatus.OK);
+        return new ResponseEntity<>(new MultiResponseDto<>(tagMapper.tagsToTagsResponse(tags), pageTags), HttpStatus.OK);
+    }
+
+    @GetMapping("/{tag-id}")
+    public ResponseEntity getTag(@PathVariable("tag-id") long tagId, @Positive @RequestParam int page, @Positive @RequestParam int size, @RequestParam String sortBy) {
+        Tag tag = tagService.findTag(tagId);
+        Page<Question> pageQuestions = questionService.findQuestions(tag, page - 1, size, sortBy);
+        List<Question> questions = pageQuestions.getContent();
+
+        return new ResponseEntity<>(new TagQuestionListResponseDto(tagMapper.tagTotagResponse(tag, questions), pageQuestions), HttpStatus.OK);
     }
 
 }
