fd868802c0e864e1fd254217f35d59de49d0c8ad
DDunDDang <thdwns0331@gmail.com>
2023-04-17 17:55:21 +0900

    QuestionMapper 수정 - tag 관련 로직 추가

--- backend/src/main/java/com/dudung/preproject/question/mapper/QuestionMapper.java
+++ backend/src/main/java/com/dudung/preproject/question/mapper/QuestionMapper.java
@@ -1,9 +1,16 @@
 package com.dudung.preproject.question.mapper;
 
 import com.dudung.preproject.answer.dto.AnswerDto;
+import com.dudung.preproject.exception.BusinessLogicException;
+import com.dudung.preproject.exception.ExceptionCode;
+import com.dudung.preproject.member.domain.Member;
 import com.dudung.preproject.question.domain.Question;
 import com.dudung.preproject.question.dto.QuestionDto;
 import com.dudung.preproject.question.dto.QuestionResponseDto;
+import com.dudung.preproject.tag.domain.Tag;
+import com.dudung.preproject.tag.dto.TagDto;
+import com.dudung.preproject.tag.repository.TagRepository;
+import com.dudung.preproject.tag.service.TagService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,15 +19,36 @@ import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
-    @Mapping(source = "memberId", target = "member.memberId")
-    Question questionPostToQuestion(QuestionDto.Post requestBody);
+
+    default Question questionPostToQuestion(QuestionDto.Post requestBody) {
+        Question question = new Question();
+        Member member = new Member();
+        member.setMemberId(requestBody.getMemberId());
+
+        question.setQuestionTitle(requestBody.getQuestionTitle());
+        question.setQuestionContent(requestBody.getQuestionContent());
+        question.setCreatedAt(requestBody.getCreatedAt());
+
+        List<Tag> tagList = requestBody.getTagName().stream()
+                .map(tagName -> {
+                    Tag tag = new Tag();
+                    tag.setTagId(tagName.getTagId());
+                    return tag;
+                }).collect(Collectors.toList());
+        question.setTags(tagList);
+        question.setMember(member);
+
+        return question;
+    }
     Question questionPatchToQuestion(QuestionDto.Patch requestBody);
     default QuestionDto.Response questionToQuestionResponse(Question question) {
         QuestionResponseDto questionResponseDto = QuestionResponseDto.builder()
+                .questionId(question.getQuestionId())
                 .questionTitle(question.getQuestionTitle())
                 .questionContent(question.getQuestionContent())
                 .createdAt(question.getCreatedAt())
                 .modifiedAt(question.getModifiedAt())
+                .tagName(tagsToTagNames(question.getTags()))
                 .questionVoteSum(question.getQuestionVoteSum())
                 .viewCount(question.getViewCount())
                 .memberName(question.getMember().getName()).build();
@@ -36,12 +64,15 @@ public interface QuestionMapper {
         return new QuestionDto.Response(questionResponseDto, answerResponseDto);
     }
 
+    List<TagDto.Name> tagsToTagNames(List<Tag> tagList);
+
     default QuestionDto.ResponseForList questionToQuestionResponseForList(Question question) {
         return QuestionDto.ResponseForList.builder()
                 .questionTitle(question.getQuestionTitle())
                 .viewCount(question.getViewCount())
                 .questionVoteSum(question.getQuestionVoteSum())
                 .createdAt(question.getCreatedAt())
+                .tagName(tagsToTagNames(question.getTags()))
                 .memberName(question.getMember().getName())
                 .answerCount(question.getAnswerCount()).build();
     }
