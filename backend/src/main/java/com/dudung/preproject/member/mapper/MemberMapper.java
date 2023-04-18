07368cf24cb304493136cb77b1400f3855090bbb
swj8560 <swj5514@gmail.com>
2023-04-19 00:00:01 +0900

    MemberMapper 수정 - membersToMemberResponses 추가

--- backend/src/main/java/com/dudung/preproject/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/dudung/preproject/member/mapper/MemberMapper.java
@@ -4,10 +4,13 @@ import com.dudung.preproject.member.domain.Member;
 import com.dudung.preproject.member.dto.MemberDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member memeberPostToMember(MemberDto.Post requestBody);
 
     Member memberPatchToMember(MemberDto.Patch requestBody);
     MemberDto.Response memberToMemberResponse(Member member);
+    List<MemberDto.Response> membersToMemberResponses(List<Member> members);
 }
