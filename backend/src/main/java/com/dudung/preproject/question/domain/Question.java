31ba28a7a2e416df85c6f9b2447bdbb0822e836d
DDunDDang <thdwns0331@gmail.com>
2023-04-18 11:52:44 +0900

    Question 수정 - questionVoteSum 추가

--- backend/src/main/java/com/dudung/preproject/question/domain/Question.java
+++ backend/src/main/java/com/dudung/preproject/question/domain/Question.java
@@ -35,12 +35,15 @@ public class Question {
     @OneToMany(mappedBy = "question")
     private List<QuestionVote> questionVotes = new ArrayList<>();
 
+    private int questionVoteSum;
     public int getQuestionVoteSum() {
 
         int questionVoteSum = this.questionVotes.stream()
                 .map(answerVote -> answerVote.getQuestionVoteStatus().getScore())
                 .mapToInt(N -> N)
                 .sum();
+
+        this.questionVoteSum = questionVoteSum;
 
         return questionVoteSum;
     }
