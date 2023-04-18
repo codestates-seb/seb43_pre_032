89f7349f16370b7720a805315c61e82234063b4b
JHJFE <ghwo9611@gmail.com>
2023-04-18 10:46:27 +0900

    feature/header/jhj

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-//import GlobalStyle from './styles/GlobalStyle';
+import GlobalStyle from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
