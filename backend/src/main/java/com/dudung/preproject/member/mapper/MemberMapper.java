7bfaf99e3966e8938b96ef997c95578b41c49304
swj8560 <swj5514@gmail.com>
2023-04-19 22:46:46 +0900

    MemberMapper 수정 - return 추가

--- backend/src/main/java/com/dudung/preproject/member/mapper/MemberMapper.java
+++ backend/src/main/java/com/dudung/preproject/member/mapper/MemberMapper.java
@@ -5,12 +5,31 @@ import com.dudung.preproject.member.dto.MemberDto;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
     Member memeberPostToMember(MemberDto.Post requestBody);
 
     Member memberPatchToMember(MemberDto.Patch requestBody);
-    MemberDto.Response memberToMemberResponse(Member member);
-    List<MemberDto.Response> membersToMemberResponses(List<Member> members);
+    default MemberDto.Response memberToMemberResponse(Member member){
+        return MemberDto.Response.builder()
+                .memberId(member.getMemberId())
+                .name(member.getName())
+                .build();
+    }
+
+    default MemberDto.ResponseForList memberToMemberResponseForList(Member member) {
+        return MemberDto.ResponseForList.builder()
+                .memberId(member.getMemberId())
+                .name(member.getName())
+                .build();
+
+    }
+
+    default List<MemberDto.ResponseForList> membersToMemberResponses(List<Member> members) {
+        return members.stream()
+                .map(member -> memberToMemberResponseForList(member))
+                .collect(Collectors.toList());
+    }
 }
