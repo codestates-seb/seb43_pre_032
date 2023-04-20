29f79b4f48bd9746a8e94006d526f98185877b8c
JHJFE <ghwo9611@gmail.com>
2023-04-20 10:35:09 +0900

    feature/router-store-modify-setting/jhj

--- client/src/components/Header/HeaderButton.js
+++ client/src/components/Header/HeaderButton.js
@@ -3,15 +3,37 @@ import achieve from '../../assets/achieve.png';
 import event from '../../assets/event.png';
 import help from '../../assets/help.png';
 import info from '../../assets/info.png';
+// import { selectNav } from '../../store/store';
+// import { useDispatch } from 'react-redux';
+
+const OtherButtons = () => {
+  // const dispatch = useDispatch();
+  const example = [{ data1: '1' }, { data2: '2' }];
+
+  return (
+    <OtherContainer className="flex-center">
+      <Menu>
+        <Profile>호재</Profile>
+        {example.length}
+      </Menu>
+      <Menu>
+        <img className="other-img-size" src={event} alt="이벤트" />
+      </Menu>
+      <Menu>
+        <img className="other-img-size" src={achieve} alt="성취" />
+      </Menu>
+      <Menu>
+        <img className="other-img-size" src={help} alt="도움" />
+      </Menu>
+      <Menu>
+        <img className="other-img-size" src={info} alt="기타정보" />
+      </Menu>
+    </OtherContainer>
+  );
+};
+
 export const LoginBtn = styled.div`
   height: 30px;
-  border: 1px solid var(--login-btn-border);
-  border-radius: 4px;
-  background-color: var(--login-btn-before);
-  padding-left: 3px;
-  padding-right: 3px;
-  color: var(--login-btn-border);
-  margin: 5px;
   :hover {
     background-color: var(--login-btn-after);
   }
@@ -21,14 +43,6 @@ export const SignInBtn = styled.div`
   height: 30px;
   font-weight: bold;
   width: 60px;
-  border: 1px solid skyblue;
-  border-radius: 4px;
-  box-shadow: 0px 0.5px 1px#dadce0 inset;
-  border: 1px solid skyblue;
-  background-color: var(--signup-btn-before);
-  padding-left: 7px;
-  padding-right: 7px;
-  color: white;
   :hover {
     background-color: var(--signup-btn-after);
   }
@@ -60,28 +74,5 @@ const Profile = styled.div`
   color: white;
   font-size: 10px;
 `;
-const OtherButtons = () => {
-  const example = [{ data1: '1' }, { data2: '2' }];
-  return (
-    <OtherContainer className="flex-center">
-      <Menu>
-        <Profile>호재</Profile>
-        {example.length}
-      </Menu>
-      <Menu>
-        <img className="other-img-size" src={event} alt="이벤트" />
-      </Menu>
-      <Menu>
-        <img className="other-img-size" src={achieve} alt="성취" />
-      </Menu>
-      <Menu>
-        <img className="other-img-size" src={help} alt="도움" />
-      </Menu>
-      <Menu>
-        <img className="other-img-size" src={info} alt="기타정보" />
-      </Menu>
-    </OtherContainer>
-  );
-};
 
 export { OtherButtons };
---
+++ client/src/pages/CreateAsk.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+const CreateContainer = styled.section``;
+const CreateAsk = () => {
+  return CreateContainer;
+};
+export default CreateAsk;
