7b39783d7282b659e494f36005619c29a7fee43d
DDunDDang <thdwns0331@gmail.com>
2023-04-20 22:36:48 +0900

    Feat: OAuth2.0 GOOGLE

---
+++ backend/src/main/java/com/dudung/preproject/auth/handler/OAuth2MemberSuccessHandler.java
@@ -0,0 +1,94 @@
+package com.dudung.preproject.auth.handler;
+
+import com.dudung.preproject.auth.jwt.JwtTokenizer;
+import com.dudung.preproject.auth.utils.CustomAuthorityUtils;
+import com.dudung.preproject.member.domain.Member;
+import com.dudung.preproject.member.service.MemberService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
+    private final MemberService memberService;
+
+    public OAuth2MemberSuccessHandler(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils, MemberService memberService) {
+        this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
+        this.memberService = memberService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        var oAuth2User = (OAuth2User)authentication.getPrincipal();
+        String email = String.valueOf(oAuth2User.getAttributes().get("email"));
+        List<String> authorities = authorityUtils.createRoles(email);
+
+        saveMember(email);
+        redirect(request, response, email, authorities);
+    }
+
+    private void saveMember(String email) {
+        Member member = new Member(email);
+        memberService.createMember(member);
+    }
+
+    private void redirect(HttpServletRequest request, HttpServletResponse response, String username, List<String> authorities) throws IOException {
+        String accessToken = delegateAccessToken(username, authorities);
+        String refreshToken = delegateRefreshToken(username);
+        String uri = createUri(accessToken, refreshToken).toString();
+        getRedirectStrategy().sendRedirect(request, response, uri);
+    }
+    private String delegateAccessToken(String username, List<String> authorities) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", username);
+        claims.put("roles", authorities);
+
+        String subject = username;
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(String username) {
+        String subject = username;
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+
+    private URI createUri(String accessToken, String refreshToken) {
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("access_token", accessToken);
+        queryParams.add("access_token", refreshToken);
+
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host("localhost")
+                .path("/receive-token.html")
+                .queryParams(queryParams)
+                .build()
+                .toUri();
+
+    }
+
+}
