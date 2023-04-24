d1f9f2867ba154851434c20f954bbeea50d172dc
mscojl24 <mscojl24@naver.com>
2023-04-25 07:56:07 +0900

    feat: question 버튼(list-filter) axios 요청 기능 수정

--- client/src/pages/DetailQuestion.js
+++ client/src/pages/DetailQuestion.js
@@ -16,7 +16,7 @@ function DetailQuestion() {
   useEffect(() => {
     axios
       .get(
-        `http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:8080/questions/${qsId.qsId}?page=1&size=5&sortBy=answerId`,
+        `http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:8080/questions/${qsId.qsId}?page=1&sortBy=answerId`,
         {
           headers: {
             'ngrok-skip-browser-warning': '69420',
