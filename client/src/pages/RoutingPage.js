2c0f6884b9474929d5ae43236715bd59fd47a838
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-04-24 13:56:08 +0900

    tag-질문리스트 이동

--- client/src/pages/RoutingPage.js
+++ client/src/pages/RoutingPage.js
@@ -43,8 +43,8 @@ const Paging = () => {
               <Route path="/" element={<Navigate replace to="/members" />} />
               <Route path="/members" element={<Signup />} />
               <Route path="/auth/login" element={<Login />} />
-              <Route path="/detailquestion" element={<DetailQuestion />} />
               <Route path="/question" element={<Viewcomponent />} />
+              <Route path="/question/:qId" element={<DetailQuestion />} />
               <Route path="/users/3355" element={<MyinfoPage />} />
               <Route path="/users/edit/3355" element={<MyInfoEdit />} />
               <Route path="/question/ask" element={<CreateAsk />} />
