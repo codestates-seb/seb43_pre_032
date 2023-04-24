77af9721cfc1588b66577fe076f5ddc8e04c8e5b
swj8560 <swj5514@gmail.com>
2023-04-25 00:03:40 +0900

    Fix: authenticationMemberId

--- backend/src/main/java/com/dudung/preproject/answer/service/AnswerService.java
+++ backend/src/main/java/com/dudung/preproject/answer/service/AnswerService.java
@@ -4,6 +4,13 @@ import com.dudung.preproject.answer.domain.Answer;
 import com.dudung.preproject.answer.repository.AnswerRepository;
 import com.dudung.preproject.exception.BusinessLogicException;
 import com.dudung.preproject.exception.ExceptionCode;
+import com.dudung.preproject.member.domain.Member;
+import com.dudung.preproject.member.repository.MemberRepository;
+import com.dudung.preproject.member.service.MemberService;
+import com.dudung.preproject.question.domain.Question;
+import com.dudung.preproject.question.repository.QuestionRepository;
+import com.dudung.preproject.question.repository.QuestionTagRepository;
+import com.dudung.preproject.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.Page;
@@ -19,20 +26,30 @@ import java.util.Optional;
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
+    private final MemberService memberService;
 
+    public Answer createAnswer(Answer answer, long authenticationMemberId) {
+        checkVerifiedId(authenticationMemberId);
+        Member member = memberService.findMember(authenticationMemberId);
+        answer.setMember(member);
 
-    public Answer createAnswer(Answer answer) {
-
-        return answerRepository.save(answer);
+        Answer createdAnswer = answerRepository.save(answer);
+        member.addAnswer(createdAnswer);
+        return createdAnswer;
     }
 
-    public Answer updateAnswer(Answer answer) {
+    public Answer updateAnswer(Answer answer, long authenticationMemberId) {
+        checkVerifiedId(authenticationMemberId);
         Answer findAnswer = findVerifiedAnswer(answer.getAnswerId());
+
+        patchPermission(findAnswer, memberService.findMember(authenticationMemberId));
 
         Optional.ofNullable(answer.getQuestion())
                 .ifPresent(question -> findAnswer.setQuestion(question));
         Optional.ofNullable(answer.getModifiedAt())
                 .ifPresent(LocalDateTime -> findAnswer.setModifiedAt(java.time.LocalDateTime.now()));
+
+        findAnswer.setModifiedAt(answer.getModifiedAt());
 
         return answerRepository.save(findAnswer);
     }
@@ -45,16 +62,37 @@ public class AnswerService {
     }
 
     public Answer findAnswer(Long answerId) {
-        return findVerifiedAnswer(answerId);
+
+        Answer findedAnswer = findVerifiedAnswer(answerId);
+
+        return findedAnswer;
     }
 
-    public void deleteAnswer(Long answerId) {
+    public void deleteAnswer(Long answerId, long authenticationMemberId) {
+        checkVerifiedId(authenticationMemberId);
         Answer findAnswer = findVerifiedAnswer(answerId);
+        deletePermission(findAnswer, memberService.findMember(authenticationMemberId));
         answerRepository.delete(findAnswer);
     }
 
     public Page<Answer> findAnswers(int page, String sortBy) {
         return answerRepository.findAll(PageRequest.of
                 (page, 5, Sort.by(sortBy).descending()));
+    }
+
+    private void checkVerifiedId(long authenticationMemeberId) {
+        if (authenticationMemeberId == -1) throw new BusinessLogicException(ExceptionCode.UNAUTHORIZED);
+    }
+
+    private void patchPermission(Answer answer, Member member) {
+        if (!answer.getMember().getMemberId().equals(member.getMemberId())) {
+            throw new BusinessLogicException(ExceptionCode.ONLY_AUTHOR);
+        }
+    }
+
+    private void deletePermission(Answer answer, Member member) {
+        if (!answer.getMember().getMemberId().equals(member.getMemberId()) && !member.getEmail().equals("admin@gmail.com")) {
+            throw new BusinessLogicException(ExceptionCode.ONLY_AUTHOR);
+        }
     }
 }
