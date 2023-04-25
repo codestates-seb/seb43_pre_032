aa7c55dc43744ef024ef10b7a2158b360f0ff052
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-04-25 21:23:39 +0900

    답변의 댓글 crud 구현

--- client/src/components/DetailQuestion/Comment/CommentUpdate.js
+++ client/src/components/DetailQuestion/Comment/CommentUpdate.js
@@ -8,33 +8,68 @@ function CommentUpdate({
   qsId,
   useEffectget,
   setUpdateCommentOn,
+  isAnswerComment,
+  answerId,
 }) {
   const token = localStorage.getItem('token');
+  const memberId = localStorage.getItem('memberid');
   const handleUpdateContent = (e) => {
     setUpdateContent(e.target.value);
   };
+  const handlekeydown = (event) => {
+    if (event.key === 'Enter') {
+      handleUpdateContentClick(updateId);
+    }
+  };
   const handleUpdateContentClick = (updateId) => {
-    axios
-      .patch(
-        `http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:8080/questionanswers/${updateId}`,
-        {
-          questionAnswerId: Number(updateId),
-          questionId: Number(qsId.qsId),
-          questionAnswerContent: updateContent,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: token,
+    if (!isAnswerComment) {
+      axios
+        .patch(
+          `http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:8080/questionanswers/${updateId}`,
+          {
+            questionAnswerId: Number(updateId),
+            questionId: Number(qsId.qsId),
+            questionAnswerContent: updateContent,
           },
-        }
-      )
-      .then((response) => {
-        console.log(response);
-        useEffectget();
-        setUpdateCommentOn(false);
-      })
-      .catch((error) => console.error(error));
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: token,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          useEffectget();
+          setUpdateCommentOn(false);
+        })
+        .catch((error) => console.error(error));
+    } else {
+      axios
+        .patch(
+          `http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:8080/answeranswers/${updateId}`,
+          {
+            answerAnswerId: Number(updateId),
+            memberId: memberId,
+            answerId: answerId,
+            answerAnswerContent: updateContent,
+            modifiedAt: new Date(),
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: token,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          useEffectget();
+          setUpdateCommentOn(false);
+        })
+        .catch((error) => console.error(error));
+      window.location.reload();
+    }
   };
   return (
     <CommentWrite>
@@ -43,6 +78,7 @@ function CommentUpdate({
         placeholder={updateContent}
         value={updateContent}
         onChange={handleUpdateContent}
+        onKeyDown={handlekeydown}
       ></input>
       <button onClick={() => handleUpdateContentClick(updateId)}>submit</button>
     </CommentWrite>
