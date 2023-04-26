98ddd1bc71087ae9b615770dbc3ba65d4d69e014
DDunDDang <thdwns0331@gmail.com>
2023-04-26 14:25:24 +0900

    Fix: CORS

--- backend/src/main/java/com/dudung/preproject/auth/config/SecurityConfiguration.java
+++ backend/src/main/java/com/dudung/preproject/auth/config/SecurityConfiguration.java
@@ -82,7 +82,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.addExposedHeader("MemberId");
@@ -152,6 +152,7 @@ public class SecurityConfiguration implements WebMvcConfigurer {
                 .allowedOrigins("http:localhost:3000")
                 .allowedOrigins("http:localhost:3001")
                 .allowedOrigins("https://2e9b-61-254-8-200.ngrok-free.app")
+                .allowedOrigins("http://ec2-13-125-39-247.ap-northeast-2.compute.amazonaws.com:3000")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용합니다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
