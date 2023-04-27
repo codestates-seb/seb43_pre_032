9801ad6a09f57d293d4dc865468344690fd37da5
LEE2302 <dltpdudqkqh1@gmail.com>
2023-04-27 16:54:57 +0900

    feat: oauth 1차 통합

--- client/src/pages/QusetionList.jsx
+++ client/src/pages/QusetionList.jsx
@@ -1,8 +1,24 @@
 import Sidebanner from '../components/question/Sidebanner.jsx';
 import Questions from '../components/question/Questions.jsx';
+import { useDispatch } from 'react-redux';
+import { selectFooter, selectNav } from '../store/store';
 import styled from 'styled-components';
 
 function Viewcomponent({ tagId }) {
+  const searchParamsToken = new URLSearchParams(window.location.search);
+  const token = searchParamsToken.get('Authorization');
+  const searchParamsId = new URLSearchParams(window.location.search);
+  const memberid = searchParamsId.get('Memberid');
+
+  const dispatch = useDispatch();
+
+  if (token && memberid) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('memberid', memberid);
+    dispatch(selectFooter(true));
+    dispatch(selectNav(true));
+  }
+
   return (
     <Questionscomponent>
       <Viewquestion>
